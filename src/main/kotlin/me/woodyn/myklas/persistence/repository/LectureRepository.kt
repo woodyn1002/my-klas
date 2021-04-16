0402958222768fab53f9265148f6606094b791fa
woodyn1002 <woodyn1002@gmail.com>
2021-04-16 16:33:49 +0900

    Apply optimistic locks on the lecture registering

--- src/main/kotlin/me/woodyn/myklas/persistence/repository/LectureRepository.kt
+++ src/main/kotlin/me/woodyn/myklas/persistence/repository/LectureRepository.kt
@@ -3,11 +3,19 @@ package me.woodyn.myklas.persistence.repository
 import me.woodyn.myklas.persistence.model.Lecture
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import java.util.*
+import javax.persistence.LockModeType
 
 @Repository
 interface LectureRepository : JpaRepository<Lecture, Long>, JpaSpecificationExecutor<Lecture> {
 
     fun findAllByTerm(term: String): List<Lecture>
+
+    @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+    @Query("select lec from Lecture lec where lec.id = ?1")
+    fun findByIdForUpdate(id: Long): Optional<Lecture>
 
 }
