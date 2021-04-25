5f8451f6ccca65664e1729aa2b11872ca9e91c32
woodyn1002 <woodyn1002@gmail.com>
2021-04-25 23:43:18 +0900

    Fix mechanism of caching full lectures not to use additional threads

--- src/main/kotlin/me/woodyn/myklas/MyKlasApplication.kt
+++ src/main/kotlin/me/woodyn/myklas/MyKlasApplication.kt
@@ -3,11 +3,9 @@ package me.woodyn.myklas
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
 @EnableJpaRepositories
-@EnableAsync
 class MyKlasApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/me/woodyn/myklas/service/FullLectureArchive.kt
+++ src/main/kotlin/me/woodyn/myklas/service/FullLectureArchive.kt
@@ -2,27 +2,33 @@ package me.woodyn.myklas.service
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
+import java.time.Clock
 import java.util.concurrent.ConcurrentHashMap
 
 @Component
 class FullLectureArchive {
-    private val fullLectureIds: MutableSet<Long> = ConcurrentHashMap.newKeySet()
+    private val fullLectureIds: MutableMap<Long, Long> = ConcurrentHashMap()
+    private val clock: Clock = Clock.systemDefaultZone()
 
     val logger: Logger get() = LoggerFactory.getLogger(this.javaClass)
 
-    fun isFull(lectureId: Long) =
-        lectureId in fullLectureIds
+    fun isFull(lectureId: Long): Boolean {
+        val expTime = fullLectureIds[lectureId] ?: return false
+        if (expTime <= clock.millis()) {
+            val removed = fullLectureIds.remove(lectureId, expTime)
+            if (removed) {
+                logger.debug("Invalidate capacity cache of lecture $lectureId")
+            }
+            return !removed
+        }
+        return true
+    }
 
-    @Async
     fun setFullFor(lectureId: Long, duration: Long) {
-        fullLectureIds += lectureId
+        val expTime = clock.millis() + duration
+        fullLectureIds[lectureId] = expTime
         logger.debug("Set lecture $lectureId full")
-
-        Thread.sleep(duration)
-        fullLectureIds -= lectureId
-        logger.debug("Invalidate capacity cache of lecture $lectureId")
     }
 
 }
