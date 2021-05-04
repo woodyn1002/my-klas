8f6b4ffa806566deb6961919708a7c035b01f13d
woodyn1002 <woodyn1002@gmail.com>
2021-05-04 18:42:33 +0900

    Reduce locking time of a lecture when registering

--- src/main/kotlin/me/woodyn/myklas/service/LectureRegistrationService.kt
+++ src/main/kotlin/me/woodyn/myklas/service/LectureRegistrationService.kt
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
 
 @Service
 class LectureRegistrationService(
@@ -20,6 +22,9 @@ class LectureRegistrationService(
     private val registrationRepository: LectureRegistrationRepository,
     private val studentRepository: StudentRepository,
     private val lectureRepository: LectureRepository,
+
+    @PersistenceContext
+    private val entityManager: EntityManager,
 
     @Autowired(required = false)
     @Qualifier("basic")
@@ -37,7 +42,7 @@ class LectureRegistrationService(
         val student = studentRepository.findByIdForUpdate(studentId).orElseThrow {
             errorHelper.notFound("Student $studentId")
         }
-        val lecture = lectureRepository.findByIdForUpdate(dto.lectureId).orElseThrow {
+        val lecture = lectureRepository.findById(dto.lectureId).orElseThrow {
             errorHelper.notFound("Lecture ${dto.lectureId}")
         }
 
@@ -50,18 +55,23 @@ class LectureRegistrationService(
             }
         }
 
-        logger.debug("Lecture ${lecture.id} has ${lecture.numAvailable} seats available")
-        if (lecture.numAvailable <= 0) {
+        entityManager.detach(lecture)
+        val lectureForUpdate = lectureRepository.findByIdForUpdate(dto.lectureId).orElseThrow {
+            errorHelper.notFound("Lecture ${dto.lectureId}")
+        }
+
+        logger.debug("Lecture ${lectureForUpdate.id} has ${lectureForUpdate.numAvailable} seats available")
+        if (lectureForUpdate.numAvailable <= 0) {
             throw errorHelper.badRequest(
                 "Student couldn't register the lecture: no available seats"
             )
         }
-        lecture.numAvailable--
-        logger.debug("The remaining seats of the lecture ${lecture.id} is ${lecture.numAvailable}")
+        lectureForUpdate.numAvailable--
+        logger.debug("The remaining seats of the lecture ${lectureForUpdate.id} is ${lectureForUpdate.numAvailable}")
 
         val registration = LectureRegistration(
             student = student,
-            lecture = lecture
+            lecture = lectureForUpdate
         )
         registrationRepository.save(registration)
 
